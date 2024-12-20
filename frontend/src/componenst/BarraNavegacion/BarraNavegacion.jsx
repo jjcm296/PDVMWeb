95fa96ae45509c24f6572259806a2f6091d1c220
jjcm296 <jordanjaircruzmendoza@gmail.com>
2024-12-19 19:53:36 -0600

    add navigation bar

---
+++ frontend/src/componenst/BarraNavegacion/BarraNavegacion.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link} from "react-router-dom";
+import "./BarraNavegacion.css"
+
+const BarraNavegacion = () => {
+    return (
+        <div className="BarraNavegacion">
+            <div className="menu">
+                <nav>
+                    <ul className="items">
+                        <li id="item">🛒 PDV</li>
+                        <li id="item">📝 Inventario</li>
+                        <li id="item">🚚 Suministro</li>
+                        <li id="item">🔔 Alertas</li>
+                        <li id="item">🏷️ Productos</li>
+                        <li id="item">👤 Usuario</li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="menu">
+                <nav>
+                    <ul className="items">
+                        <li id="item">🔔</li>
+                        <li id="item">📱</li>
+                        <li id="item">❓</li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    );
+};
+
+export default BarraNavegacion;
