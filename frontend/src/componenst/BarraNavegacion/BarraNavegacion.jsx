98f47b5f3849df1f94387cb2c271c1f9ff94e6e3
jjcm296 <jordanjaircruzmendoza@gmail.com>
2024-12-21 18:48:32 -0600

    Add new navigate bar

--- frontend/src/componenst/BarraNavegacion/BarraNavegacion.jsx
+++ frontend/src/componenst/BarraNavegacion/BarraNavegacion.jsx
@@ -1,18 +1,51 @@
-import React from 'react';
-import { Link} from "react-router-dom";
-import "./BarraNavegacion.css"
+import React, { useState, useRef, useEffect } from 'react';
+import "./BarraNavegacion.css";
+import ModalIniciarSesion from "../modals/modalIniciarSesion/ModalIniciarSesion";
 
 const BarraNavegacion = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalPosition, setModalPosition] = useState({ top: 0, left: 0 });
+    const loginButtonRef = useRef(null);
+    const modalRef = useRef(null);
+
+    const closeModal = () => setIsModalOpen(false);
+
+    const handleButtonClick = () => {
+        if (isModalOpen) {
+            setIsModalOpen(false);
+        } else {
+            if (loginButtonRef.current) {
+                const rect = loginButtonRef.current.getBoundingClientRect(); // Obtener las coordenadas del botón
+                setModalPosition({
+                    top: rect.bottom + window.scrollY + 5,
+                    left: rect.left + window.scrollX - 150,
+                });
+            }
+            setIsModalOpen(true);
+        }
+    };
+    // Cerrar el modal si el clic es fuera del modal
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (modalRef.current && !modalRef.current.contains(event.target) && !loginButtonRef.current.contains(event.target)) {
+                setIsModalOpen(false); // Cerrar el modal
+            }
+        };
+        // Agregar el evento de clic al documento
+        document.addEventListener("mousedown", handleClickOutside);
+        // Limpiar el evento al desmontar el modal
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+
     return (
         <div className="BarraNavegacion">
             <div className="menu">
-                <nav>
+                <nav className="home">
                     <ul className="items">
-                        <li id="item">🛒 PDV</li>
-                        <li id="item">📝 Inventario</li>
-                        <li id="item">🚚 Suministro</li>
-                        <li id="item">🔔 Alertas</li>
-                        <li id="item">🏷️ Productos</li>
+                        <li id="item">HOME</li>
                     </ul>
                 </nav>
             </div>
@@ -22,11 +55,15 @@ const BarraNavegacion = () => {
                         <li id="item">🔔</li>
                         <li id="item">📱</li>
                         <li id="item">❓</li>
-                        <button className="login-button">
+                        <button
+                            ref={loginButtonRef}
+                            onClick={handleButtonClick}
+                            className="login-button">
                         </button>
                     </ul>
                 </nav>
             </div>
+            {isModalOpen && <ModalIniciarSesion ref={modalRef} position={modalPosition} onClose={closeModal} />}
         </div>
     );
 };
