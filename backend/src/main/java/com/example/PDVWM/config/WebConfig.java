cf05f478b1ddbd01381979d67d44ab77270a5d4d
jjcm296 <153791495+jjcm296@users.noreply.github.com>
2025-06-05 06:10:41 +0000

    ahora si xd

--- backend/src/main/java/com/example/PDVWM/config/WebConfig.java
+++ backend/src/main/java/com/example/PDVWM/config/WebConfig.java
@@ -8,13 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
+         registry.addMapping("/**")
+                .allowedOriginPatterns(
                     "http://localhost:3000",
                     "https://pdv-web-psi.vercel.app",
                     "http://192.168.1.*:3000",
                     "https://*.app.github.dev"
-                    )
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
