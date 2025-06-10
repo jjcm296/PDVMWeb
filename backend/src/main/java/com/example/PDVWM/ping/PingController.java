97cc3fc4aa226a32beecd2fb3fb1d9497cdf55e5
jjcm296 <jordanjaircruzmendoza@gmail.com>
2025-06-10 10:07:56 -0600

    feat: se agrego la importacion correcta de requestmaping

--- backend/src/main/java/com/example/PDVWM/ping/PingController.java
+++ backend/src/main/java/com/example/PDVWM/ping/PingController.java
@@ -2,6 +2,7 @@ package com.example.PDVWM.ping;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/api")
